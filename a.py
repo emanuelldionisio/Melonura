8fc8b454ab36476a6da44218e0462c16fefce704
Emanuel Eduardo Oliveira Dionisio <emanuelodionisio@gmail.com>
2026-05-26 20:18:15 +0000

    ;;;

--- a.py
+++ a.py
@@ -1,5 +1,10 @@
 from pydub import AudioSegment
-from playsound import playsound
 
-audio = AudioSegment.from_file("hehe.mp3")
-playsound("hehe.mp3")
+# 1m 6aa 3m
+
+audio = AudioSegment.from_file("todos_as_notas_dos_sapos.mp3")
+a = audio[:250]
+b = audio[90000:90500]
+c = audio[4000:4250] 
+novo_audio = a + b + c
+novo_audio.export("novo_audio.mp3", format="mp3")
